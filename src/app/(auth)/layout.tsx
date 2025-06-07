9af72d2734788eb3cef4cbccf3d3c3a35060599f
rhgiddp <rhgiddp@naver.com>
2025-06-07 14:35:26 +0900

    tailwind css v4 에 맞줘 정리 -> 적용안 되던거 적용되도록

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,6 @@ interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
-
 export default function AuthLayout({children}: AuthLayoutProps) {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
